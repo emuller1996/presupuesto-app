11881e9bc8254d11f397caf9ff3d7a492d3cf74f
emuller1996 <stefano.muller@hotmail.com>
2024-04-17 00:35:07 -0500

    end point todas las facturas

--- server/src/routes/factura.routes.js
+++ server/src/routes/factura.routes.js
@@ -1,15 +1,25 @@
 const { Router } = require("express");
-const {
-    createFactura
-} = require("../controllers/facturaController.js");
+const { createFactura } = require("../controllers/facturaController.js");
+const { Factura, Contracto, Proyecto } = require("../db.js");
 
 const FacturaRouter = Router();
 
 FacturaRouter.post("/", createFactura);
-
-
-
-
-
+FacturaRouter.get("/", async (req, res) => {
+  try {
+    const facturas = await Factura.findAll({
+      include: [
+        {
+          model: Contracto,
+          include: Proyecto,
+        },
+      ],
+    });
+    return res.status(200).json(facturas);
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+});
 
 module.exports = FacturaRouter;
